65e33e111c10c127d98c8fcc92df152d4151ecba
henrylarsen <henrylarsen01@yahoo.ca>
2021-03-18 19:47:34 -0700

    Started scraper

--- Model/wpost_scraper.py
+++ Model/wpost_scraper.py
@@ -1,1 +1,17 @@
-// SCRAPER FILE -- write to SQL
+## SCRAPER FILE -- write to SQL
+
+import scrapy
+from scrapy import Request
+
+from dateutil.parser import parse
+
+
+class WpostScraperSpider(scrapy.Spider):
+    name = 'wpost_scraper'
+    allowed_domains = ['wsj.com']
+
+    def start_requests(self):
+        url = 'https://www.wsj.com/news/archive/{}'
+        for year in range(1995, 2000):
+            str i = url.format(year)
+        
