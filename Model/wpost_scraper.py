362dfa8b8902f9fe270a0a8fedc42f93369fbabf
Cameron Holland <cameronholland2883@gmail.com>
2021-03-18 21:11:10 -0600

    MakeList completed

--- Model/wpost_scraper.py
+++ Model/wpost_scraper.py
@@ -9,9 +9,31 @@ from dateutil.parser import parse
 class WpostScraperSpider(scrapy.Spider):
     name = 'wpost_scraper'
     allowed_domains = ['wsj.com']
+    list_date = []:
+
+    def make_list():
+        for year in range(1995, 2020):
+            for month in range(1,12):
+                if (month == 2):
+                    if (year%4 == 0):
+                        for day in range(1, 29):
+                            date = year + "/" + str(month).zfill(2) + "/" + str(day).zfill(2)
+                            list_date.add(date)
+                    else:
+                        for day in range(1, 28):
+                            date = year + "/" + str(month).zfill(2) + "/" + str(day).zfill(2)
+                            list_date.add(date)
+                else if (month < 8):
+                    for day in range(1, 30 + month%2):
+                        str date = year + "/" + str(month).zfill(2) + "/" + str(day).zfill(2)
+                        list_date.add(date)
+                else:
+                    for day in range(1, 31 - month%2):
+                        str date = year + "/" + str(month).zfill(2) + "/" + str(day).zfill(2)
+                        list_date.add(date)
 
     def start_requests(self):
         url = 'https://www.wsj.com/news/archive/{}'
-        for year in range(1995, 2000):
-            str i = url.format(year)
+        list_date = make_list()
+        str i = url.format(year)
         
